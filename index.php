c4c22c2ab14e39fdb288d339599af665a7553e7d
Nicolas Cosnard <nicolas@cosnard.eu>
2013-12-05 17:53:54 +0100

    Added Bootstrap & jQuery

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Let's go !</title>
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <h1>Bienvenue sur la page de la nuit de l'info de la team let's go !</h1>
+
+        <h2> C'est trop cool elle est vide ! </h2>
+
+        <script src="bootstrap/js/jquery.js"></script>
+        <script src="bootstrap/js/bootstrap.js"></script>
+    </body>
+</html>
