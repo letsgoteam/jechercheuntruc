e292dd3c9e059bdab8b4d926ba0b4a4e222ffd42
gregoire.poinsot@ensi-bourges.fr <krispix90@PapaOurs.(none)>
2013-12-05 21:28:07 +0100

    DataBase question & tag update

--- letsgo.sql
+++ letsgo.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Jeu 05 Décembre 2013 à 20:48
--- Version du serveur: 5.5.34-MariaDB-log
--- Version de PHP: 5.5.6
+-- Généré le : Jeu 05 Décembre 2013 à 20:57
+-- Version du serveur: 5.5.34
+-- Version de PHP: 5.3.10-1ubuntu3.8
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données: `letsgo`
@@ -39,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   `contenu` text NOT NULL,
   `datetime` bigint(20) NOT NULL,
   `auteur` int(11) NOT NULL,
-  `best_rep` int(11) NOT NULL,
+  `best_rep` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `auteur` (`auteur`),
   KEY `best_rep` (`best_rep`)
@@ -71,7 +77,8 @@ CREATE TABLE IF NOT EXISTS `tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tag` varchar(255) NOT NULL,
   `count` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tag` (`tag`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -103,8 +110,8 @@ ALTER TABLE `asso_question_tag`
 -- Contraintes pour la table `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `question_ibfk_2` FOREIGN KEY (`best_rep`) REFERENCES `reponse` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`auteur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`auteur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `question_ibfk_2` FOREIGN KEY (`best_rep`) REFERENCES `reponse` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `reponse`
@@ -112,3 +119,7 @@ ALTER TABLE `question`
 ALTER TABLE `reponse`
   ADD CONSTRAINT `reponse_ibfk_2` FOREIGN KEY (`auteur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`question`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
