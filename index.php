96a791699b3eecd8dab5af15850369940932a5a8
Nicolas Cosnard <nicolas@cosnard.eu>
2013-12-05 18:16:52 +0100

    Updated meta tags & added container div

--- index.php
+++ index.php
@@ -2,21 +2,25 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Let's go !</title>
+        <meta name="author" content="Let's Go team"/>
+        <meta name="robots" content="all"/>
+        <title>La cave de Papa Ours</title>
         <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
         <link href="bootstrap/css/bootstrap-theme.css" rel="stylesheet">
         <link href="papaOurs.css" rel="stylesheet">
     </head>
     <body>
+        <div class="container col-lg-12">
+            <h1>La cave de Papa Ours</h1>
+            <?php
+            if (isset($_GET['src'])) {
+                include "src.php";
+            } else {
+                include "accueil.php";
+            }
+            ?>
 
-        <h1>La cave de Papa Ours</h1>
-        <?php
-        if (isset($_GET['src'])) {
-            include "src.php";
-        } else {
-            include "accueil.php";
-        }
-        ?>
+        </div>
 
 
 
