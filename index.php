a0d2162f58ba7400443aafbe0b18268dcd9e1a6b
nuitinfo <nuitinfo@nuitinfo-EB1006.(none)>
2013-12-06 06:50:02 +0100

    Added reply display on reply.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$dbh = new PDO('mysql:host=localhost;dbname=letsgo', 'letsgo', 'nuitinfo');
+$dbh = new PDO('mysql:host=localhost;dbname=letsgo', 'root', 'nuitinfo');
 
 $connect = FALSE;
 $failconnect = FALSE;
@@ -27,12 +27,16 @@ if (isset($_GET['page']) && $_GET['page'] == 'logout') {
         }
     }
 }
+
+if(isset($_GET['page']) && $_GET['page'] == "reply" && !isset($_GET['tid'])) {
+	header('Location: index.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
+        <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="author" content="Let's Go team"/>
--- thread.php
+++ thread.php
@@ -6,3 +6,30 @@
  * and open the template in the editor.
  */
 
+if(isset($_GET['tid']) && $_GET['tid'] != "") {
+	$query1 = 'SELECT * FROM `question` WHERE `id` = ?';
+	
+	$q1 = $dbh->prepare($query1);
+	$q1->execute(array($_GET['tid']));
+
+	$return1 = $q1->fetch();
+
+	echo '<div class="col-xs-12 result"><span class="glyphicon glyphicon-question-sign"></span> <strong>' . $return1['titre'] . '</strong><br />';
+        echo '<p>' . nl2br($return1['contenu']) . '</p>';
+        echo '<span class="glyphicon glyphicon-flag"></span> <strong>Tags :</strong> ';
+        echo '</div>';
+	// End of question display
+
+	$query2 = 'SELECT reponse.contenu, utilisateur.username FROM reponse INNER JOIN utilisateur ON reponse.auteur = utilisateur.id WHERE question = ?';
+	$q2 = $dbh->prepare($query2);
+	$q2->execute(array($_GET['tid']));
+
+
+	foreach($q2->fetchAll() as $answ):
+		echo '<div class="col-xs-12 result"><span class="glyphicon glyphicon-chevron-right"></span> <strong>' . $answ['username'] . '</strong><br />';
+		echo '<p>' . nl2br($answ['contenu']) . '</p>';
+        	echo '</div>';
+	endforeach;
+}
+
+?>
